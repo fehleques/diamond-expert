f1c6bbe79de384bbea6e52d71f28483fad7dac0a
fehleques <fehleques@gmail.com>
2023-04-04 21:49:49 -0300

    FAQ section

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// $('.navbar_menu-icon-wrapper').on('click', function() {
+//     $('.navbar_menu-wrapper').toggleClass('is--open');
+//   });
+
+//   $(window).scroll(function () {
+//     $(window).scrollTop() > 100 ? $(".navbar").addClass("on--scroll") : $(".navbar").removeClass("on--scroll");
+//   });
+
+// $('.faq_item-wrap').on('click', function() {
+//     $(this).removeClass('is--clicked'),
+//     $(this).toggleClass('is--clicked');
+//   });
+
+$(".faq_item-wrap").removeClass("is--clicked");
+
+$(".faq_item-wrap").on("click", function () {
+  $(this).toggleClass("is--clicked");
+  $(this).siblings(".is--clicked").removeClass("is--clicked");
+});
